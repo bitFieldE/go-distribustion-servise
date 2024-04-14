7aee152d9a062f61d14765e7ecad7becdb430db8
RkAirforce <rk.state.of.the.art@gmail.com>
2024-04-14 13:08:39 +0900

    ADD: セグメントの作成

---
+++ WriteALogPackage/internal/log/store.go
@@ -0,0 +1,92 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenwidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+// レシーバを使ってメソッドチェーンで使えるようにする
+// ファイル名と同じレシーバを設けると統一性の取れたコードが書ける
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	// 排他制御
+	// goroutineによる並行処理で共有リソースにアクセスしなければならないとき、排他制御が必要になる。
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenwidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenwidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenwidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
+}
---
+++ WriteALogPackage/internal/log/store_test.go
@@ -0,0 +1,63 @@
+package log
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func testAppend(t *testting.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenwidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, lenwidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n)
+		off += int64(n)
+	}
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|as.O_APPEND,
+		0600,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
